06b5e298f06db2ddd24e320be6e4691a6c3367cf
Dev.BPerez <PapiP3@users.noreply.github.com>
2025-05-15 04:46:59 -0400

    ch7: manually configure Tailwind CSS

---
+++ app/dashboard/Sidebar.tsx
@@ -0,0 +1,7 @@
+<nav>
+  <ul>
+    <li><a href="/dashboard">Dashboard</a></li>
+    <li><a href="/dashboard/invoices">Invoices</a></li>
+    <li><a href="/dashboard/customers">Customers</a></li>
+  </ul>
+</nav>
